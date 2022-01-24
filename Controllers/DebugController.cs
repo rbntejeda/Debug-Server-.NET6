875c87e1836deffa9d5ee4af6e291285630c422c
Ruben <rubentejedaroa@gmail.com>
2022-01-23 21:44:28 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/DebugController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Debug.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebugController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
